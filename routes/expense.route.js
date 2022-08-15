cb92d582d1aa20ce630ad473a08e6f2c6fdb7024
mshassan96 <miansalmanhassan@yahoo.com>
2022-08-15 16:34:30 +0500

    Added middleware

--- routes/expense.route.js
+++ routes/expense.route.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
 
 const { getAll, getOne } = require("../controllers/expense.controller");
 
 // ROUTE 1: Fetch all expenses using: GET "api/expenses/getAll".
-router.get("/", getAll);
+router.get("/", fetchuser, getAll);
 
 // ROUTE 2: Fetch expense by id using: GET "api/expenses/id/:id".
-router.get("/id/:id", getOne);
+router.get("/id/:id", fetchuser, getOne);
 
 
 module.exports = router;
